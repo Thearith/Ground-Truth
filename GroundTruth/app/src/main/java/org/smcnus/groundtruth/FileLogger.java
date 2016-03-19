e7f2d426dea6877eb4e805eb18dc8ff5b39ad4cc
thearith <sothearith.sreang@gmail.com>
2016-03-19 21:10:43 +0800

    implement the full flow of application

---
+++ GroundTruth/app/src/main/java/org/smcnus/groundtruth/FileLogger.java
@@ -0,0 +1,115 @@
+package org.smcnus.groundtruth;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FileLogger {
+
+    // constants
+    private static final String TAG                     = FileLogger.class.getSimpleName();
+
+    private static final String GROUND_TRUTH_FOLDER     = "iRace Heel Strike";
+    private static final String LOG_FILE_EXTENSION      = ".txt";
+    private static final String SEPERATOR               = "\t";
+    private static final String TIMESTAMP_LOG_FORMAT    = // index timestamp
+            "%d" + SEPERATOR + "%s";
+    private static final String TIMESTAMP_FORMAT        = "MMMM dd yyyy | HH:mm:ss:SSS";
+
+    private static FileLogger fileLogger;
+    private ArrayList<String> logs;
+
+    /*
+    * Constructor
+    * */
+
+    public static FileLogger getInstance() {
+        if(fileLogger == null) {
+            fileLogger = new FileLogger();
+        }
+
+        return fileLogger;
+    }
+
+    private FileLogger() {
+        logs = new ArrayList<>();
+    }
+
+
+    /*
+    * public methods
+    * */
+
+    public void writeLogsToFile(ArrayList<Long> timestamps) {
+        long firstTimestamp = timestamps.get(0);
+        String recordDirPath = createGroundTruthFolderIfNotExist();
+        String fileName = recordDirPath + "/" + getFileName(firstTimestamp) + LOG_FILE_EXTENSION;
+        File logFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
+
+        Log.d(TAG, fileName);
+
+        addTimestampsToLog(timestamps);
+
+        try {
+            if (!logFile.exists())
+                logFile.createNewFile();
+
+            BufferedWriter outputStreamWriter = new BufferedWriter(new FileWriter(logFile));
+
+            for(int index=0; index<logs.size(); index++) {
+                String log = logs.get(index) + "\n";
+                outputStreamWriter.write(log);
+            }
+
+            outputStreamWriter.close();
+            logs.clear();
+
+        } catch (IOException e) {
+            Log.e(TAG, "File write failed: " + e.toString());
+        }
+    }
+
+    private void addTimestampsToLog(ArrayList<Long> timestamps) {
+        for(int index=0; index<timestamps.size(); index++)
+            addTimestampToLog(index, timestamps.get(index));
+    }
+
+    private void addTimestampToLog(int index, long timestamp) {
+        String log = String.format(TIMESTAMP_LOG_FORMAT, index, String.valueOf(timestamp));
+        logs.add(log);
+    }
+
+
+    /*
+    * Helper method
+    * */
+
+    private String getFileName(long timestamp) {
+        return "Ground Truth - " + getDateFormat(timestamp);
+    }
+
+    private String getDateFormat(long timestamp) {
+        Date date = new Date(timestamp);
+        SimpleDateFormat formatter = new SimpleDateFormat(TIMESTAMP_FORMAT);
+
+        return formatter.format(date);
+    }
+
+    private String createGroundTruthFolderIfNotExist() {
+        String recordDirPath = GROUND_TRUTH_FOLDER;
+        File recordDirectory =
+                new File(Environment.getExternalStorageDirectory().getAbsolutePath(), recordDirPath);
+
+        if(!recordDirectory.exists())
+            recordDirectory.mkdirs();
+
+        return recordDirPath;
+    }
+}
--- GroundTruth/app/src/main/java/org/smcnus/groundtruth/MainActivity.java
+++ GroundTruth/app/src/main/java/org/smcnus/groundtruth/MainActivity.java
@@ -8,8 +8,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String SINGPAORE_TIMEZONE = "Asia/Singapore";
+
+    private FileLogger fileLogger;
+
+    private TextView stepsCountTextView;
+
+    private Calendar calendar;
+
+    private ArrayList<Long> timestampList;
+    private int stepCount;
+
+
+    /*
+    * Activity methods
+    * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +49,132 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initializeFileLogger();
+        initializeCalendar();
+        initializeData();
+        initializeWidgets();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /*
+    * overridden methods
+    * */
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.recordTimestampBtn:
+                long timestamp = calendar.getTimeInMillis();
+                addTimestampList(timestamp);
+                incrementStepCount();
+                break;
+
+            case R.id.stopButton:
+                writeLogsToFile();
+                makeToast();
+                break;
+        }
+    }
+
+
+    /*
+    * File logger
+    * */
+
+    private void initializeFileLogger() {
+        fileLogger = FileLogger.getInstance();
+    }
+
+    private void writeLogsToFile() {
+        if(timestampList.size() > 0) {
+            fileLogger.writeLogsToFile(timestampList);
+        }
+    }
+
+
+    /*
+    * Calendar methods
+    * */
+
+    private void initializeCalendar() {
+        TimeZone timeZone = TimeZone.getTimeZone(SINGPAORE_TIMEZONE);
+        calendar = Calendar.getInstance(timeZone);
+    }
+
+    /*
+    * Data methods
+    * */
+
+    private void initializeData() {
+        initializeStepCount();
+        initializeTimestampList();
+    }
+
+    private void initializeStepCount() {
+        stepCount = 0;
+    }
+
+    private void initializeTimestampList() {
+        timestampList = new ArrayList<>();
+    }
+
+    private void incrementStepCount() {
+        stepCount += 1;
+        updateStepCountTextView();
+    }
+
+    private void addTimestampList(long timestamp) {
+        timestampList.add(timestamp);
+    }
+
+
+    /*
+    * Widgets method
+    * */
+
+    private void initializeWidgets() {
+        initializeStepCountTextView();
+        initializeRecordButton();
+        initializeStopButton();
+    }
+
+    private void initializeStepCountTextView() {
+        stepsCountTextView = (TextView) findViewById(R.id.stepsCountTextView);
+    }
+
+    private void updateStepCountTextView() {
+        stepsCountTextView.setText(String.valueOf(stepCount));
+    }
+
+    private void initializeRecordButton() {
+        LinearLayout recordTimestampBtn = (LinearLayout) findViewById(R.id.recordTimestampBtn);
+        recordTimestampBtn.setOnClickListener(this);
+    }
+
+    private void initializeStopButton() {
+        LinearLayout stopButton = (LinearLayout) findViewById(R.id.stopButton);
+        stopButton.setOnClickListener(this);
+    }
+
+    private void makeToast() {
+        Toast.makeText(this, "Recording Finished", Toast.LENGTH_SHORT).show();
     }
 }
